4a0a4ea4321b01c26300804fc89b50a41010f41e
hammesa <hammesa@iastate.edu>
2017-05-18 18:16:34 -0400

    Use `readFile` for reading input.txt

--- e254/e254.hs
+++ e254/e254.hs
@@ -1,6 +1,5 @@
 import Data.List
 import Data.Maybe
-import System.IO
 
 mapping :: [(Char, Char)]
 mapping = zip ['a'..'z'] ['z', 'y'..]
@@ -13,8 +12,7 @@ atbashCipher = map charComplement
 
 main :: IO ()
 main = do
-    handle <- openFile "input.txt" ReadMode
-    contents <- hGetContents handle
+    contents <- readFile "input.txt"
     let cipherText = atbashCipher contents
     putStr cipherText
 
