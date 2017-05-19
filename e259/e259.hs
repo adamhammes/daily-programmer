9d9e955db91278a3264d94cf5d9421a2a6e44246
Adam Hammes <adam@hammes.io>
2017-05-19 18:01:42 -0500

    Challenge #259 [Easy] Clarence the Slow Typist

--- e259/e259.hs
+++ e259/e259.hs
@@ -1,30 +1,29 @@
-import Data.List
-import Data.Maybe
-import Text.Printf
+import Data.List (elemIndex, foldl')
+import Data.Maybe (mapMaybe)
+import Text.Printf (printf)
 
 type Point = (Int, Int)
 
-getCoord :: Char -> Maybe (Int, Int)
+getCoord :: Char -> Maybe Point
 getCoord c = do
     index <- elemIndex c "123456789.0"
     return (index `mod` 3, index `div` 3)
 
-pointDist :: (Point, Point) -> Double
-pointDist ((x1, y1), (x2, y2)) = sqrt (fromIntegral $ dx ^ 2 + dy ^ 2)
+pointDist :: Point -> Point -> Double
+pointDist (x1, y1) (x2, y2) = sqrt (fromIntegral $ dx ^ 2 + dy ^ 2)
     where dx = x1 - x2
           dy = y1 - y2
 
 readString :: String -> [Point]
-readString s = catMaybes $ map getCoord s
+readString = mapMaybe getCoord
 
 typingDistance :: String -> Double
 typingDistance s = foldl' (+) 0 distances
-    where distances = map pointDist pairs
-          pairs = zip points (tail points)
+    where distances = zipWith pointDist points (drop 1 points)
           points = readString s
 
-printTypingDistance :: String -> String
-printTypingDistance s = printf "%0.2f\n" (typingDistance s)
-
 main :: IO ()
-main = interact printTypingDistance
+main = do
+    line <- getLine
+    putStrLn $ printf "  -> %0.2fcm" (typingDistance line)
+    main
