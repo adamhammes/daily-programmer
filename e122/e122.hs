8b6dfd0263c7e8c56ba241cfe181fb7b694c54d5
Adam Hammes <adam@hammes.io>
2017-05-23 23:08:19 -0500

    Challenge #122 [Easy] Sum Them Digits

---
+++ e122/e122.hs
@@ -0,0 +1,16 @@
+digits :: Integer -> [Integer]
+digits 0 = []
+digits i = digits (i `div` 10) ++ [i `mod` 10]
+
+digitSum :: Integer -> Integer
+digitSum i
+  | i `div` 10 == 0 = i
+  | otherwise = digitSum $ sum (digits i)
+
+inputs :: [(Integer, Integer)]
+inputs = [(31337, 8), (1073741824, 1)]
+
+main :: IO ()
+main = (print . and) $ map check inputs
+  where
+    check (input, answer) = answer == digitSum input
