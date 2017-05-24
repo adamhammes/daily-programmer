60ee2fadf301cc50fc35d7d9b8f6fade2037568d
Adam Hammes <adam@hammes.io>
2017-05-24 14:28:07 -0500

    Small readability improvements

--- i140/i140.hs
+++ i140/i140.hs
@@ -3,13 +3,15 @@ import Data.Ix
 import Data.List
 import qualified Data.Set as Set
 
-genGrid :: Integer -> Set.Set (Integer, Integer) -> [[Bool]]
+type Edge = (Integer, Integer)
+
+genGrid :: Integer -> Set.Set Edge -> [[Bool]]
 genGrid n edges = (map . map) (`Set.member` edges) grid
   where
     grid = groupBy ((==) `on` fst) coords
     coords = range ((0, 0), (n - 1, n - 1))
 
-parseEdge :: String -> [(Integer, Integer)]
+parseEdge :: String -> [Edge]
 parseEdge s = [(from, to) | from <- outgoing, to <- incoming]
   where
     outgoing = map read (takeWhile (/= "->") split)
@@ -17,14 +19,12 @@ parseEdge s = [(from, to) | from <- outgoing, to <- incoming]
     split = words s
 
 doChallenge :: [String] -> [String]
-doChallenge s = map (map pprint) (genGrid n edges)
+doChallenge s = (map . map) pprint (genGrid n edges)
   where
     n = read (head $ words $ head s)
     edges = Set.fromList $ concatMap parseEdge (drop 1 s)
-    pprint b =
-      if b
-        then '1'
-        else '0'
+    pprint True = '1'
+    pprint _ = '0'
 
 input :: [String]
 input = ["5 5", "0 -> 1", "1 -> 2", "2 -> 4", "3 -> 4", "0 -> 3"]
